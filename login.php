f0ac0d9a3a79d38bb09ecb7ed3b10411d33bacc0
Nik Rozman <rozman.nik@gmail.com>
2019-05-03 13:55:39 +0200

    Update login.php

--- login.php
+++ login.php
@@ -39,7 +39,9 @@ else {
         height="72">
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
       <label for="useremail" class="sr-only">Username</label>
-      <input type="text" id="useremail" class="form-control" placeholder="Username" required="" autofocus="" autocomplete="off">
+      <input type="plain" id="useremail" class="form-control" placeholder="Username" required="" autofocus=""
+        autocomplete="off"
+        style="background-image: url(&quot;data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAAAXNSR0IArs4c6QAAAPhJREFUOBHlU70KgzAQPlMhEvoQTg6OPoOjT+JWOnRqkUKHgqWP4OQbOPokTk6OTkVULNSLVc62oJmbIdzd95NcuGjX2/3YVI/Ts+t0WLE2ut5xsQ0O+90F6UxFjAI8qNcEGONia08e6MNONYwCS7EQAizLmtGUDEzTBNd1fxsYhjEBnHPQNG3KKTYV34F8ec/zwHEciOMYyrIE3/ehKAqIoggo9inGXKmFXwbyBkmSQJqmUNe15IRhCG3byphitm1/eUzDM4qR0TTNjEixGdAnSi3keS5vSk2UDKqqgizLqB4YzvassiKhGtZ/jDMtLOnHz7TE+yf8BaDZXA509yeBAAAAAElFTkSuQmCC&quot;); background-repeat: no-repeat; background-attachment: scroll; background-size: 16px 18px; background-position: 98% 50%; cursor: auto;">
       <label for="password" class="sr-only">Password</label>
       <input type="password" name="password" id="password" class="form-control" placeholder="Password" required=""
         autocomplete="off"
