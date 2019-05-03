8e6f540807b4e1208e99b83d0642f042011868bc
Nik Rozman <rozman.nik@gmail.com>
2019-05-03 14:57:36 +0200

    Update login.php

--- login.php
+++ login.php
@@ -34,8 +34,9 @@ else {
       <div class="form-group">
         <img class="mb-4" src="https://accuratenode.com/assets/img/glyph-1080.png" alt="" width="72" height="72">
         <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-        <label for="username" class="sr-only">Username</label><br>
+        <label for="username" class="sr-only">Username</label>
         <input type="username" name="username" id="username" class="form-control" placeholder="Username" required="" autocomplete="off">
+        <br>
         <label for="password" class="sr-only">Password</label>
         <input type="password" name="password" id="password" class="form-control" placeholder="Password" required="" autocomplete="off">
         <button class="btn btn-lg btn-primary btn-block" name="submit" type="submit">Sign in</button>
