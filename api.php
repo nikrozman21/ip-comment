a8ca3be1dff6be0cdc82ca3acbfb36a7ec1f57cd
Nik Rozman <rozman.nik@gmail.com>
2019-05-03 03:18:11 +0200

    Update api.php

--- api.php
+++ api.php
@@ -5,17 +5,15 @@ session_name(NAME);
 				$useremail = $_POST['useremail'];
 				$password = $_POST['password'];
                 $userData = $odb->query("SELECT * FROM `users` WHERE `username`='$useremail'")->fetch();
-                if (SHA1(md5($password)) === $userData['password']) {
 					if (SHA1(md5($password)) === $userData['password']) {
 						session_start();
 						$_SESSION['id'] = $userData['id'];
                         $_SESSION['username'] = $userData['username'];
                         header("LOCATION: admin");
 					} else {
-						header("LOCATION: admin");
+						header("LOCATION: login");
 					}
                 }
-            }
 /*         	break;
         case 'upd-comment':
             $odb->query('UPDATE IPs SET comment="' . $_POST['comment'] . '" WHERE ip = "' . $_POST['ip'] . '"')->fetch();
