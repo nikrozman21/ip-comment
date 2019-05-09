258d5f2cbe5cdf0eeee896611c4aae2ac37c04dc
Nik Rozman <rozman.nik@gmail.com>
2019-05-09 18:46:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -30,12 +30,9 @@ if (isset($_SESSION['id'])) {
     <?php include 'nav.php'; ?>
     <main role="main" class="container">
         <div class="jumbotron text-center">
-            <h1 class="display-3"><b><span class="text-primary mb-4">IP</span>Info</b></h1>
-            <p class="lead">Welcome to AccurateNode's IP administration panel.<br>This panel is built mostly for staff
-                to
-                keep track of the used IPs and what they are in use by.</p>
-            <a class="btn btn-sm btn-primary" href="https://accuratenode.com/" target="_blank" role="button">Landing
-                page</a>
+            <h1 class="display-3 mb-4"><b><span class="text-primary font-weight-bold">IP</span>Info</b></h1>
+            <p class="lead mb-4">Welcome to AccurateNode's IP administration panel.<br>This panel is built mostly for staff to keep track of the used IPs and what they are in use by.</p>
+            <a class="btn btn-sm btn-primary" href="https://accuratenode.com/" target="_blank" role="button">Landing page</a>
         </div>
     </main>
 </body>
