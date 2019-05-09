80f744c1518d273f1c5231d3fa12448d976d6827
Nik Rozman <rozman.nik@gmail.com>
2019-05-09 18:13:38 +0200

    navbar

--- nav.php
+++ nav.php
@@ -1,5 +1,5 @@
     <nav class="fixed-top navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#"><b><span class="text-primary">IP</span>Info</b></a>
+        <a class="navbar-brand" href="https://ipinfo.accuratenode.com"><b><span class="text-primary">IP</span>Info</b></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -8,7 +8,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
+                    <a class="nav-link" href="https://ipinfo.accuratenode.com">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="api/plain">Plain</a>
