5237c8ec1ff03b330d7d159d90f31d1b4146616d
Nik Rozman <rozman.nik@gmail.com>
2019-05-09 17:31:11 +0200

    Fix

--- login.php
+++ login.php
@@ -18,31 +18,35 @@ else {
 <!DOCTYPE html>
 <html>
 
-  <head>
-    <title>Login | IP management</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-      integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
-    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-    <link href="css/signin.css" rel="stylesheet">
-  </head>
+<head>
+  <title>Login | IP management</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+  <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+  <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+  <link href="css/signin.css" rel="stylesheet">
+</head>
 
-  <body class="text-center">
+<body>
   <?php include 'nav.php'; ?>
+  <div class="text-center">
     <form action="api.php" method="post">
       <div class="form-group">
         <img class="mb-4" src="https://accuratenode.com/assets/img/glyph-1080.png" alt="" width="72" height="72">
         <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
         <label for="username" class="sr-only">Username</label>
-        <input type="username" name="username" id="username" class="form-control username" placeholder="Username" required="" autocomplete="off">
+        <input type="username" name="username" id="username" class="form-control username" placeholder="Username"
+          required="" autocomplete="off">
         <label for="password" class="sr-only">Password</label>
-        <input type="password" name="password" id="password" class="form-control password" placeholder="Password" required="" autocomplete="off">
+        <input type="password" name="password" id="password" class="form-control password" placeholder="Password"
+          required="" autocomplete="off">
         <button class="btn btn-lg btn-primary btn-block space" name="submit" type="submit">Sign in</button>
-        <p class="mt-5 mb-3 text-muted">© 2019</p>
+        <p class="mt-5 mb-3 text-muted">AccurateNode © 2019</p>
       </div>
-      </form>
-  </body>
+    </form>
+  </div>
+</body>
 
 </html>
