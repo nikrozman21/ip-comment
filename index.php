904eb3a73e677b79d84611ba7897f5cd4fb1ffed
Nik Rozman <rozman.nik@gmail.com>
2019-05-09 17:15:06 +0200

    Added universal Navbar

--- index.php
+++ index.php
@@ -29,28 +29,7 @@ if (isset($_SESSION['id'])) {
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#"><b><span class="text-primary">IP</span>Info</b></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="api/plain">Plain</a>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <a href="admin" target="_blank" class="btn btn-warning my-2 my-sm-0" role="button">Admin Area</a>
-            </form>
-        </div>
-    </nav>
-
+<?php include 'nav.php'; ?>
     <div class="container">
         <div class="alert alert-primary" role="alert">
             This website uses cookies! Or does it ??
---
+++ nav.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#"><b><span class="text-primary">IP</span>Info</b></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="api/plain">Plain</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <a href="admin" target="_blank" class="btn btn-warning my-2 my-sm-0" role="button">Admin Area</a>
+        </form>
+    </div>
+</nav>
